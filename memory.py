9606919c77f20de2140e9057dcf28630722848a2
Ana Flores <a01735115@tec.mx>
2024-05-09 17:56:38 -0600

    Se cambio lo de centrar el número en el tablero

--- memory.py
+++ memory.py
@@ -73,9 +73,10 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 26, y + 2.5)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark], align=('center'), font=('Arial', 30, 'normal'))
+        
 
     update()
     ontimer(draw, 100)
